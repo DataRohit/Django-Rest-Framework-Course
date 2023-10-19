b2f2b5908be07491b5a02651104dccc97445e0f2
datarohit <rohit.vilas.ingole@gmail.com>
2023-10-19 10:18:14 +0530

    Add Product View and URL Added

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -5,17 +5,13 @@ from products.models import Product
 
 # Define a serializer for the Product model
 class ProductSerializer(serializers.ModelSerializer):
+    # Include properties explicitly
+    sale_price = serializers.ReadOnlyField()
+    discount = serializers.ReadOnlyField()
+
     class Meta:
         # Specify the model to be serialized
         model = Product
 
         # Serialize all fields in the model
-        fields = [
-            "id",
-            "title",
-            "category",
-            "description",
-            "price",
-            "sale_price",
-            "discount",
-        ]
+        fields = "__all__"
