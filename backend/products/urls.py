090c61350c4f1a5723341354129afc7069f73953
Rohit Ingole <rohit.vilas.ingole@gmail.com>
2023-10-23 19:47:13 +0530

    Search URL and View Added

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     ),
     path("list/", views.ProductListView.as_view(), name="list_product"),
     path("create/", views.ProductCreateView.as_view(), name="create_product"),
+    path("search/", views.ProductSearchView.as_view(), name="search_product"),
 ]
