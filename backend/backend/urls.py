eb369308c83bf8ed898eadaec5eead1ac19de939
Rohit Ingole <rohit.vilas.ingole@gmail.com>
2023-10-24 11:29:30 +0530

    Home Path Fixed

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,22 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework.response import Response
-from rest_framework import status
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path(
-        "",
-        lambda request: Response(
-            {
-                "message": "Hello from the DataRohit!",
-            },
-            status=status.HTTP_200_OK,
-        ),
-    ),
+    path("", include("restapi.urls")),
     path("jwtauth/", include("jwtauth.urls")),
     path("tokenauth/", include("tokenauth.urls")),
-    path("restapi/", include("restapi.urls")),
     path("products/", include("products.urls")),
 ]
